107e7285c741885a4b81c36bb99cbcee86d6f168
Soledad Dato <soledato@gmail.com>
2025-08-22 15:03:55 -0300

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,66 @@
+import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
+import icon from "astro-icon";
+
+import vercel from "@astrojs/vercel";
+;
+
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+    
+  },
+  output: 'server',
+  adapter: vercel(),
+  
+
+  experimental: {
+    fonts: [{
+      provider: "local",
+      name: "InterLocal",
+      cssVariable: "--font-inter",
+      variants: [
+        {
+          weight: 400,
+          style: "normal",
+          src: ["./src/assets/fonts/Inter-Regular.woff2"]
+        },
+        {
+          weight: 600,
+          style: "normal",
+          src: ["./src/assets/fonts/Inter-SemiBold.woff2"]
+        },
+        {
+          weight: 700,
+          style: "normal",
+          src: ["./src/assets/fonts/Inter-Bold.woff2"]
+        }
+      ]
+    },
+    {
+      provider: "local",
+      name: "InterLocalDisplay",
+      cssVariable: "--font-inter-display",
+      variants: [
+        {
+          weight: 400,
+          style: "normal",
+          src: ["./src/assets/fonts/InterDisplay-Regular.woff2"]
+        },
+        {
+          weight: 500,
+          style: "normal",
+          src: ["./src/assets/fonts/InterDisplay-Medium.woff2"]
+        },
+        {
+          weight: 600,
+          style: "normal",
+          src: ["./src/assets/fonts/InterDisplay-SemiBold.woff2"]
+        }
+      ]
+    }]
+  },
+
+  integrations: [icon()],
+  adapter: vercel(),
+})
